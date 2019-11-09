909d7875edc3f21a0f16d71a2dcdada7b4ae61b8
VictorRodB <victorrb20@gmail.com>
2019-11-09 17:46:44 -0300

    final commit

---
+++ src/RegularPrice.java
@@ -0,0 +1,15 @@
+package src;
+
+class RegularPrice extends Price {
+    
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+
+    double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+           result += (daysRented - 2) * 1.5;
+        return result;
+     }
+}
